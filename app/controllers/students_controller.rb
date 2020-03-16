3837f342f810def4c7147755a3fc067da3d2fcb2
tiburon <thechanmoon@gmail.com>
2020-03-16 00:36:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,8 +11,23 @@ class StudentsController < ApplicationController
   def new
   end
 
+  # def create
+  #   Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  #   redirect_to students_path
+  # end
+
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
+  end
+  # def create
+  #   session[:form_params] = params.inspect
+  #   redirect_to new_student_path
+  # end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
